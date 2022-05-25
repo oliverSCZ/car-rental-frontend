da4e73f0750437ecde489979e184e025d976ebc5
Oliver <olivercoimbra@gmail.com>
2022-05-25 10:06:17 -0400

    Add car reducer, add car store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,12 +2,16 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import favoritesReducer from './favorites/favorites';
+import carsReducer from './cars/cars';
 
 const reducer = combineReducers({
   favoritesReducer,
+  carsReducer,
   // additional reducers could be added here
 });
 
-const store = createStore(reducer, applyMiddleware(thunk, logger));
-
+const store = createStore(
+  reducer, applyMiddleware(thunk, logger)
+  );
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 export default store;
