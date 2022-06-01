031f399454197e65bfb279a8f865c4f63c85ed06
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2022-05-31 19:41:59 -0500

    Add style to input fields

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -50,13 +50,13 @@ const RegisterPage = () => {
         <div className="flex justify-center m-5 mb-20">
           <p className="text-base w-2/3 font-medium">Welcome! We are happy to have to here!</p>
         </div>
-        <div className="container mx-auto flex flex-col w-4/5">
+        <div className="container mx-auto flex flex-col w-4/6">
           <form onSubmit={handleSubmit}>
 
             <div className="container flex flex-col">
-              <input type="text" placeholder="Enter Username" name="username" className="text-center text-xl p-3 border rounded-full mb-5" required />
+              <input type="text" placeholder="Enter Username" name="username" className="bg-gray-100/[.7] border-2 border-slate-300 text-center text-xl p-3 rounded-full mb-5" required />
 
-              <input type="password" placeholder="Enter Password" name="psw" className="text-orange-600 text-center text-xl p-3 border rounded-full mb-5" required />
+              <input type="password" placeholder="Enter Password" name="psw" className="bg-gray-100/[.7] border-2 border-slate-300 text-orange-600 text-center text-xl p-3 rounded-full mb-5" required />
 
               <button className="rounded-full bg-orange-600 text-white w-1/2 mx-auto text-lg p-3" type="submit">Create account</button>
             </div>
