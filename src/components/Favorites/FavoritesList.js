176bf0749840d2868ae3b05b019b2fdfb128d5ca
Richard Tonata <rtonata@gmail.com>
2022-06-02 23:39:29 +0200

    add styling for favourites

--- src/components/Favorites/FavoritesList.js
+++ src/components/Favorites/FavoritesList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Slider from 'react-slick';
 import { getFavorites, deleteFavourite } from '../../redux/favorites/favorites';
 import Favorite from './Favorite';
 
@@ -24,9 +25,18 @@ const FavoritesList = () => {
 
   const cars = useSelector((state) => state.carsReducer);
 
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    infinite: true,
+    centerPadding: '50px',
+    slidesToShow: 1,
+    speed: 500,
+  };
+
   const favorites = favoritesSelector.map((favorite) => {
     const currentCar = cars.filter(
-      (car) => car.id === parseInt(favorite.car_id, 10),
+      (car) => car.id === parseInt(favorite.car_id, 10)
     )[0];
     return (
       <Favorite
@@ -41,7 +51,7 @@ const FavoritesList = () => {
     <div className="p-10 pt-20">
       <h1 className="text-3xl font-bold">My Favorites</h1>
       <div className="container mx-auto flex flex-col">FAVS</div>
-      <>{favorites}</>
+      <Slider {...settings}>{favorites}</Slider>
     </div>
   );
 };
