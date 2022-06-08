104507d418d3a71e183f0c050f7a1a57a1174026
Richard Tonata <rtonata@gmail.com>
2022-06-08 19:07:16 +0200

    Add props validation for the UserMessage component

--- src/components/UserMessage.js
+++ src/components/UserMessage.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const UserMessage = ({ props }) => {
-  const { userMessage } = props;
+const UserMessage = ({ message }) => {
+  const { userMessage } = message;
 
   return (
     <div>
@@ -14,6 +14,13 @@ const UserMessage = ({ props }) => {
       </p>
     </div>
   );
+};
+
+UserMessage.propTypes = {
+  message: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default UserMessage;
