9f145265c5bdc3640e7675ff756e793ca7c9b5f8
J. Oliver Coimbra <olivercoimbra@gmail.com>
2023-09-28 15:19:12 -0400

    Update endpoints whit new render api link

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,4 +1,4 @@
-export const HOST_URL = 'https://stormy-lake-55546.herokuapp.com';
+export const HOST_URL = 'https://car-rental-ui-eszl.onrender.com';
 // export const HOST_URL = 'http://127.0.0.1:3001'; // **uncomment for testing**
 
 export const CARS_ENDPOINT = `${HOST_URL}/cars`;
